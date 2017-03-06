ef79fdbf082ea27a0bc225960a38117e0ba3ec28
Dakshina Scott <dakshina.scott@gmail.com>
2017-03-06 12:26:47 +0000

    Adds togglable menu

---
+++ main.js
@@ -0,0 +1,9 @@
+window.onload = function() {
+  // Toggle menu
+  var burger = document.getElementById("burger");
+  burger.onclick = function() {
+    this.classList.toggle('close-icon');
+    var menu = document.getElementById('navbar');
+    menu.classList.toggle('mobile-hidden');
+  }
+}
