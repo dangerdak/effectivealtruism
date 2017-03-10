32f853a4f93a32355283fccad03f6d3135ca0211
Dakshina Scott <dakshina.scott@gmail.com>
2017-03-10 21:11:31 +0000

    Adds expenditure data

--- map.js
+++ map.js
@@ -2,46 +2,35 @@ google.charts.load('current', {'packages':['geochart']});
 google.charts.setOnLoadCallback(drawRegionsMap);
 
 function drawRegionsMap() {
+  var raw = [
+    ['Burundi', 200000,null,null],
+    ['Côte d\'Ivoire', 1070000,null,null],
+    ['Democratic Republic of Congo', 340000,null,4176371],
+    ['Ethiopia', 1220000,44992,null],
+    ['India',null,2227764,null],
+    ['Kenya',null,1658238,null],
+    ['Liberia', 70000,null,null],
+    ['Madagascar', 90000,null,null],
+    ['Malawi', 520000,null,4554015],
+    ['Mauritania',null,null,null],
+    ['Mozambique', 1250000,null,null],
+    ['Niger', 10000,null,null],
+    ['Nigeria',null,10870,null],
+    ['Pakistan',null,1535,null],
+    ['Rwanda', 340000,null,null],
+    ['Sudan', 370000,null,null],
+    ['Tanzania', 20000,null,null],
+    ['Tumikia',null,215890,null],
+    ['Uganda', 100000,null,null],
+    ['Vietnam',null,91519,null],
+    ['Yemen', 130000,null,null],
+    ['Zanzibar', 250000,null,null],
+    ['Zimbabwe',null,null,null]
+  ];
+  var totals = raw.map(el => [el[0], el[1] + el[2] + el[3]]).filter(el => (el[1] > 0));
+  totals.unshift(['Country', 'Total expenditure 2015']);
 
-  var data = google.visualization.arrayToDataTable([
-    ['Country', 'Lives saved'],
-    [ 'Ethiopia', 3404 ],
-    [ 'Tanzania', 1660 ],
-    [ 'Malawi', 1388 ],
-    [ 'Zanzibar', 821 ],
-    [ 'Mozambique', 704 ],
-    [ 'Uganda', 2154 ],
-    [ 'Madagascar', 705 ],
-    [ 'Niger', 587 ],
-    [ 'Rwanda', 469 ],
-    [ 'Yemen', 469 ],
-    [ 'Democratic Republic of the Congo', 533 ],
-    [ 'Côte d\'Ivoire', 235 ],
-    [ 'Burundi', 122 ],
-    [ 'Mauritania', 117 ],
-    [ 'Senegal', 23 ],
-    [ 'Sudan', 5 ],
-    [ 'Nigeria', 5 ],
-    [ 'Angola', 1 ],
-    [ 'Burkina Faso', 6 ],
-    [ 'Cambodia', 1 ],
-    [ 'Cameroon', 6 ],
-    [ 'Gambia', 3 ],
-    [ 'Ghana', 367 ],
-    [ 'Haiti', 4 ],
-    [ 'India', 4 ],
-    [ 'Indonesia', 3 ],
-    [ 'Kenya', 5 ],
-    [ 'Liberia', 3 ],
-    [ 'Mali', 3 ],
-    [ 'Namibia', 2 ],
-    [ 'Nepal', 1 ],
-    [ 'Papua New Guinea', 158 ],
-    [ 'Sao Tome and Principe', 1 ],
-    [ 'Sierra Leone', 7 ],
-    [ 'Togo', 325 ],
-    [ 'Zambia', 45 ] 
-  ]);
+  var data = google.visualization.arrayToDataTable(totals);
 
   var options = {};
 
