aef6a8c99df09b116127fd58709106f25204d981
Dakshina Scott <dakshina.scott@gmail.com>
2017-03-21 20:35:41 +0000

    Fixes #29: tidy JS

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 window.onload = function() {
+  
   // Determine browser-specific transition end event
   // From modernizr & https://davidwalsh.name/css-animation-callback
   function whichTransitionEvent(){
@@ -20,27 +21,28 @@ window.onload = function() {
   // Listen for a transition
   var transitionEndEvent = whichTransitionEvent();
 
-  // Toggle menu
+  // Toggle nav menu
   var burger = document.getElementById("burger");
-  burger.onclick = function() {
+  burger.addEventListener('click', function() {
     var menu = document.getElementById('navbar');
     this.classList.toggle('close-icon');
     menu.classList.toggle('hidden');
-  }
-  // tooltips for mobile (iphone)
+  });
+
+  // Tooltips for mobile (iphone)
   var tooltips = document.querySelectorAll('.tooltip');
   // Use event capturing:
   // Event on outer element (body) fires first,
   // then event on inner element (tooltip)
-  document.getElementById('main').addEventListener('click', function(e) {
+  document.getElementById('main').addEventListener('click', function() {
     tooltips.forEach(function(tooltip) {
       tooltip.lastChild.classList.remove('tooltip-show');
     });
   }, true);
   tooltips.forEach(function(tooltip) {
     var clickable = false;
-    tooltip.addEventListener('click', function(e) {
-      var text = e.target.lastChild;
+    tooltip.addEventListener('click', function() {
+      var text = this.lastChild;
       var visible = getComputedStyle(text).visibility === 'visible';
       // Must hover in order to click
       if (!visible && !clickable) {
@@ -53,39 +55,40 @@ window.onload = function() {
           text.classList.add('tooltip-show');
         }
       }
-    }, true);
+    });
   });
 
   // Scroll effects 
+  //
   // Back to top button
   var topButton = document.getElementById('top-button');
   topButton.addEventListener('click', function() {
     scrollTo('title', 500);
   });
-  // scrolling navbar
+
+  // Scrolling navbar
   var navButtons = document.querySelectorAll('.navbar a');
   navButtons.forEach(function(button) {
     button.addEventListener('click', function(e) {
-      var section = e.target.getAttribute('href').slice(1);
+      var section = this.getAttribute('href').slice(1);
       scrollTo(section, 400);
       e.preventDefault();
     });
   });
   var iconScience = document.getElementById('icon-science');
   var iconHeart = document.getElementById('icon-heart');
-  var introIcons = [iconScience, iconHeart];
-  introIcons.forEach(function(icon) {
-    icon.addEventListener(transitionEndEvent, function(e) {
-      e.target.style.color = '#f9c19c';
+  [iconScience, iconHeart].forEach(function(icon) {
+    icon.addEventListener(transitionEndEvent, function() {
+      this.style.color = '#f9c19c';
     });
   });
   var iconSingle = document.getElementById('icon-single');
   var iconMany = document.getElementById('icon-many');
+  var navbar = document.getElementById('navbar');
   window.onscroll = function() {
+
     //Hide navbar
-    var navbar = document.getElementById('navbar');
     navbar.classList.add('hidden');
-    var burger = document.getElementById('burger');
     burger.classList.remove('close-icon');
 
     // Hide/reveal back to top button
@@ -95,26 +98,32 @@ window.onload = function() {
     else {
       topButton.classList.remove('reveal');
     }
+
     // blindness animation
     if (inViewById('graphic-blindness')) {
       iconSingle.classList.add('reveal', 'reveal-left');
       iconMany.classList.add('reveal', 'reveal-right');
     }
     else {
-      iconSingle.classList.remove('reveal', 'reveal-left');
-      iconMany.classList.remove('reveal', 'reveal-right');
+      hideIcons(iconSingle, iconMany);
     }
+
     // intro animation
     if (inViewById('graphic-intro')) {
       iconScience.classList.add('reveal', 'reveal-left');
       iconHeart.classList.add('reveal', 'reveal-right');
     }
     else {
-      iconHeart.style.color = '#649f94';
-      iconScience.style.color = '#649f94';
-      iconScience.classList.remove('reveal', 'reveal-left');
-      iconHeart.classList.remove('reveal', 'reveal-right');
+      hideIcons(iconHeart, iconScience);
     }
+  }
+
+  function hideIcons() {
+    var args = Array.prototype.slice.call(arguments); 
+    args.forEach(function(icon) {
+      icon.style.color = '#649f94';
+      icon.classList.remove('reveal', 'reveal-left', 'reveal-right');
+    });
   }
 
   // Checks if element is in viewport
@@ -168,14 +177,4 @@ window.onload = function() {
       return [curtop];
     }
   }
-  /*
-  // Toggle source list
-  var sourceButton = document.getElementById('source-button');
-  var sourceList = document.getElementById('sources');
-  sourceButton.addEventListener('click', function(e) {
-    sourceList.classList.toggle('reveal-sources');
-    sourceButton.classList.toggle('ion-plus-circled');
-    sourceButton.classList.toggle('ion-minus-circled');
-  });
-  */
 }
