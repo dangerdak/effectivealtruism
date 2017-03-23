2ea69d084604199a28b2dc4568110ab618ac9022
Dakshina Scott <dakshina.scott@gmail.com>
2017-03-23 20:15:39 +0000

    Trying gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,34 @@
 var gulp = require('gulp');
 
-gulp.task('autoprefixer', function() {
+var sourcemaps = require('gulp-sourcemaps');
+var uglify = require('gulp-uglify');
+var concat = require('gulp-concat');
+
+// Add CSS prefixes
+gulp.task('styles', function() {
   var postcss = require('gulp-postcss');
-  var sourcemaps = require('gulp-sourcemaps');
   var autoprefixer = require('autoprefixer');
 
-  return gulp.src('./*.css')
+  return gulp.src('css/*.css')
     .pipe(sourcemaps.init())
-    .pipe(postcss([ autoprefixer()  ]))
+    .pipe(postcss([autoprefixer()]))
     .pipe(sourcemaps.write('.'))
-    .pipe(gulp.dest('./build'));
+    .pipe(gulp.dest('build/css'));
 });
+
+// Concatenate and minify JS
+gulp.task('scripts', function() {
+  return gulp.src('js/*.js')
+    .pipe(sourcemaps.init())
+    .pipe(uglify())
+    .pipe(concat('all.min.js'))
+    .pipe(sourcemaps.write('.'))
+    .pipe(gulp.dest('build/js'));
+});
+
+gulp.task('watch', function() {
+  gulp.watch('js/*.js', ['scripts']);
+  gulp.watch('css/*.css', ['styles']);
+});
+
+gulp.task('default', ['styles', 'scripts', 'watch']);
